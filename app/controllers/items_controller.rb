1f7eb1a14e1135ed048be49e640a50c99f57d9dc
shuhei1018 <nan10kanaruyxxn@gmail.com>
2020-12-10 20:01:37 +0900

    コントローラー仮実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,25 @@
 class ItemsController < ApplicationController
-  def index
+  def index   
     @item = Item.all
   end
 
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to new_item_path
+    else
+      render :index
+    end
+    
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :status_id, :delivery_burden_id, :delivery_area_id, :delivery_date_id, :price ).merge(user_id: current_user.id)
   end
 end
