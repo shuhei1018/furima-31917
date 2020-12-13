a3f2d1267ff04e27911189c36dfb4bf018ec9d0d
shuhei1018 <nan10kanaruyxxn@gmail.com>
2020-12-13 16:16:41 +0900

    Faker設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Lorem.word}
+    explanation {Faker::Lorem.sentence}
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
   end
 end
