e5fdfaec10024562d0a701b5eec8a0cbed61cc61
shuhei1018 <nan10kanaruyxxn@gmail.com>
2020-12-13 17:57:09 +0900

    テストコード準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,16 @@ FactoryBot.define do
   factory :item do
     name {Faker::Lorem.word}
     explanation {Faker::Lorem.sentence}
+    category_id  {2}
+    status_id  {2}
+    delivery_burden_id  {2}
+    delivery_area_id  {2}
+    delivery_date_id  {2}
+    price  {1000}
+    user
+    
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  describe '商品出品' do
+    context '商品出品ができるとき' do
+      it 'imageとnameとexplanationとcategory_idとstatus_idとdelivery_burden_idとdelivery_area_idとdelivery_date_idとpriceが存在すれば登録できること' do
+        expect(@item).to be_valid
+      end
+      
+    end
+
+    context '商品出品ができないとき' do
+      
+    end
+  end
 end
