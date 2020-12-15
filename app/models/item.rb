87c8ca1a0ea206776369daf7526ca34ccaf0f1d6
shuhei1018 <nan10kanaruyxxn@gmail.com>
2020-12-15 20:16:44 +0900

    微修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,13 @@ class Item < ApplicationRecord
 
   validates :name, :explanation, :image, presence: true
 
-  validates :category_id, numericality: { other_than: 1 }
-  validates :status_id, numericality: { other_than: 1 }
-  validates :delivery_burden_id, numericality: { other_than: 1 }
-  validates :delivery_area_id, numericality: { other_than: 1 }
-  validates :delivery_date_id, numericality: { other_than: 1 }
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :delivery_burden_id
+    validates :delivery_area_id
+    validates :delivery_date_id
+  end
 
   VALID_PRICE_HALF = /\A[0-9]+\z/.freeze
   validates :price, format: { with: VALID_PRICE_HALF }, length: { minimum: 3, maximum: 7 }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
