e2d566154b194eb861af4eb023307d5eb26e32e5
shuhei1018 <nan10kanaruyxxn@gmail.com>
2020-12-19 17:12:12 +0900

    リファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:destroy]
+
   def index
     @items = Item.includes(:user).order(created_at: :desc)
   end
@@ -18,12 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.destroy
+
+    if @item.destroy
       redirect_to root_path
     else
       render :show
@@ -35,4 +38,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :status_id, :delivery_burden_id, :delivery_area_id, :delivery_date_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless current_user.id == @item.user.id
+      redirect_to action: :index
+    end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
