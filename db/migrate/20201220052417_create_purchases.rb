8e5c1faaf7755d4d7f0b94e55dad81acc39e1a45
shuhei1018 <nan10kanaruyxxn@gmail.com>
2020-12-20 14:34:12 +0900

    purchaseのアソシエーション

--- db/migrate/20201220052417_create_purchases.rb
+++ db/migrate/20201220052417_create_purchases.rb
@@ -1,7 +1,8 @@
 class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
-
+      t.integer :user_id
+      t.integer :item_id
       t.timestamps
     end
   end
