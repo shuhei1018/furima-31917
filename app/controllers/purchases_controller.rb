206c41b814ffb93cd358ff8abb380cea4643fd4b
shuhei1018 <nan10kanaruyxxn@gmail.com>
2020-12-20 19:20:55 +0900

    複数テーブルへの保存実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,6 +4,18 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    
+    @purchase_address = PurchaseAddress.new(purchase_address_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def purchase_address_params
+    params.require(:purchase_address).permit(:postalcode, :prefecture_id, :municipality, :house_number, :building_name, :phonenumber).merge(user_id: current_user.id, item_id:params[:item_id])
   end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,32 +1,17 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postalcode, :prefecture, :municipality, :house_number, :building_name, :phonenumber
+  attr_accessor :postalcode, :prefecture_id, :municipality, :house_number, :building_name, :phonenumber, :user_id, :item_id
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postalcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
 
-  validates :prefecture, numericality: { other_than: 1, message: "can't be blank" }
-  validates :municipality, :house_number, presence: true
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  # validates :municipality, :house_number, presence: true
 
   def save
-    Purchase.create(user_id: user.id, item_id: item.id)
-    Address.create(postalcode: postalcode, prefecture: prefecture, municipality: municipality, house_number: house_number, building_name: building_name, phonenumber: phonenumber, purchase: purchase)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postalcode: postalcode, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building_name: building_name, phonenumber: phonenumber, purchase_id: purchase.id)
   end
 
 end
-
-# | Column | Type       | Options                                      |
-# | ------ | ---------- | -------------------------------------------- |
-# | item   | references | null: false, unique: true, foreign_key: true |
-# | user   | references | null: false, unique: true, foreign_key: true |
-
-# | Column        | Type      | Options                        |
-# | ------------- | --------- | ------------------------------ |
-# | postalcode    | string    | null: false                    |
-# | prefecture_id | integer   | null: false                    |
-# | municipality  | string    | null: false                    |
-# | house_number  | string    | null: false                    |
-# | building_name | string    |                                |
-# | phonenumber   | string    | null: false, unique: true      |
-# | purchase      | reference | null: false, foreign_key: true |
