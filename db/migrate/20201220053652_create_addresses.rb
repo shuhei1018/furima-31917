c059e9bfbee02e35357eb6ffbd57355f0770687b
shuhei1018 <nan10kanaruyxxn@gmail.com>
2020-12-20 14:47:13 +0900

    addressとpurchaseのマイグレート

--- db/migrate/20201220053652_create_addresses.rb
+++ db/migrate/20201220053652_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string  :postalcode
+      t.integer :prefecture_id
+      t.string :municipality
+      t.string :house_number
+      t.string :building_name
+      t.string :phonenumber
+      t.references  :purchase, foreign_key: true
       t.timestamps
     end
   end
