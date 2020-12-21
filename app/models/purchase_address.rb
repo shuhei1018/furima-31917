b2ebf49e10617e625aee859c96b27ed39bde2c56
shuhei1018 <nan10kanaruyxxn@gmail.com>
2020-12-21 22:37:59 +0900

    バリデーション

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -4,10 +4,11 @@ class PurchaseAddress
 
   with_options presence: true do
     validates :postalcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :municipality
+    validates :house_number
+    validates :phonenumber, format: {with: /\A\d{11}\z/, message: "Input only number"}
   end
-
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  # validates :municipality, :house_number, presence: true
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
