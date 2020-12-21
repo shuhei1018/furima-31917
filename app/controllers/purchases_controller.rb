595e726ff1b23eb83dde6fe013f93f53a490cf1e
shuhei1018 <nan10kanaruyxxn@gmail.com>
2020-12-21 19:51:46 +0900

    リファクタリング

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,14 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index]
+  before_action :move_to_root, only: [:index]
+
   def index
     @purchase_address = PurchaseAddress.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
     @purchase_address = PurchaseAddress.new(purchase_address_params)
-    @item = Item.find(params[:item_id])
     if @purchase_address.valid?
       @purchase_address.save
       redirect_to root_path
@@ -20,4 +22,13 @@ class PurchasesController < ApplicationController
   def purchase_address_params
     params.require(:purchase_address).permit(:postalcode, :prefecture_id, :municipality, :house_number, :building_name, :phonenumber).merge(user_id: current_user.id, item_id:params[:item_id])
   end
+
+  def move_to_root
+    redirect_to root_path if current_user.id == @item.user_id
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
 end
